cbcaf23005b3b94a4368f2fb9b5a840d25a137a8
Zahra Alanchari <zahraalanchari@Zahras-MacBook-Pro.local>
2024-09-19 12:34:08 +0330

    change structure for fix vercel bug

--- redux/action.ts
+++ redux/action.ts
@@ -1,7 +1,22 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+// import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchgetAllProduct = createAsyncThunk("products/allProducts",async ()=> {
-    const respons = await fetch(`http://localhost:8000/data`)
-    const data = await respons.json()
-    return data
-})
+// export const fetchgetAllProduct = createAsyncThunk("products/allProducts",async ()=> {
+//     const respons = await fetch(`http://localhost:8000/data`)
+//     const data = await respons.json()
+//     return data
+// })
+
+
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchgetAllProduct = createAsyncThunk(
+  'products/allProducts',
+  async () => {
+    const response = await fetch('/data.json');
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data.data; // Adjust according to your JSON structure
+  }
+);
