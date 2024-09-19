38400b8c7b6d850f728aabc5448699bbecb99aa5
Zahra Alanchari <zahraalanchari@Zahras-MacBook-Pro.local>
2024-09-19 12:41:59 +0330

    fix server js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Path to your db.json file
-const middlewares = jsonServer.defaults();
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json'); // Path to your db.json file
+// const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
+// server.use(middlewares);
+// server.use(router);
 
-const PORT = 8000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+// const PORT = 8000;
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on port ${PORT}`);
+// });
